6b4ad444fc526a19a7d7a9b1fd540dd7ee09924b
Ermek Nakypov <109239709+ErmekNakypov@users.noreply.github.com>
2024-12-17 21:41:47 +0600

    Ainura
    Signed-off-by: Ermek Nakypov <109239709+ErmekNakypov@users.noreply.github.com>

--- BLL/Exceptions/NotFoundException.cs
+++ BLL/Exceptions/NotFoundException.cs
@@ -7,4 +7,7 @@ public class NotFoundException : InvalidOperationException
     public NotFoundException(string message) : base(message) { }
 
     public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
+    
+    public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
 }
